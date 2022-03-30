7f4a6632bbb6b23db0f13903dd2c47a25c264553
Amarizky Yoga Pratama <amarizky02@gmail.com>
2022-03-30 17:18:25 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -30,7 +30,7 @@ $config['mailtype']     = 'html';
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/UCard/';
+$config['base_url'] = 'http://localhost/ucard/';
 
 /*
 |--------------------------------------------------------------------------
