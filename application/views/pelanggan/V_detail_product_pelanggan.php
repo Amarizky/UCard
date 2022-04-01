0022913bf79bdb5ae371a36ba7f543dabceec8e6
Reza Fabriza <58038780+RezaFab@users.noreply.github.com>
2022-04-01 20:18:53 +0700

    view tampilan pemesanan tali,kartu dan aksesoris

--- application/views/pelanggan/V_detail_product_pelanggan.php
+++ application/views/pelanggan/V_detail_product_pelanggan.php
@@ -101,90 +101,82 @@
                             </div>
                             <div class="grid-container">
                                 <div class="grid-item">
-                                    <b>Personalisasi</b>
+                                    <b>Yoyo</b>
                                     <br><br>
-                                    <div class="form-group" required>
-                                        <input type="checkbox" id="persona1" placeholder="personalisasi" name="personalisasi" value="1">
-                                        <label for="persona1">Blanko</label><br>
-                                        <input type="checkbox" id="persona2" placeholder="personalisasi" name="personalisasi" value="2">
-                                        <label for="persona2">Nomerator</label><br>
-                                        <input type="checkbox" id="persona3" placeholder="personalisasi" name="personalisasi" value="3">
-                                        <label for="persona3">Barcode</label><br>
-                                        <input type="checkbox" id="persona4" placeholder="personalisasi" name="personalisasi" value="4">
-                                        <label for="persona4">Data</label><br>
-                                        <input type="checkbox" id="persona5" placeholder="personalisasi" name="personalisasi" value="5">
-                                        <label for="persona5">Data + Foto</label>
-                                    </div>
+                                    <input type="radio" id="yoyo1" placeholder="yoyo" name="yoyo" value="1">
+                                    <label for="yoyo1">Yoyo Putar</label><br>
+                                    <input type="radio" id="yoyo2" placeholder="yoyo" name="yoyo" value="2">
+                                    <label for="yoyo2">Yoyo Standar</label><br>
+                                    <input type="radio" id="yoyo3" placeholder="yoyo" name="yoyo" value="3">
+                                    <label for="yoyo3">Yoyo Transparan</label>
                                 </div>
                                 <div class="grid-item">
-                                    <b>Coating</b>
+                                    <b>Warna</b>
                                     <br><br>
-                                    <input type="radio" id="coating1" placeholder="coating" name="coating" value="1">
-                                    <label for="coating1">Glossy</label><br>
-                                    <input type="radio" id="coating2" placeholder="coating" name="coating" value="2">
-                                    <label for="coating2">Doff</label><br>
-                                    <input type="radio" id="coating3" placeholder="coating" name="coating" value="3">
-                                    <label for="coating3">Glossy + Doff</label><br>
-                                    <input type="radio" id="coating4" placeholder="coating" name="coating" value="4">
-                                    <label for="coating3">UV</label>
+                                    <input type="radio" id="warna1" placeholder="warna" name="warna" value="1">
+                                    <label for="warna1">Hitam</label><br>
+                                    <input type="radio" id="warna2" placeholder="warna" name="warna" value="2">
+                                    <label for="warna2">Putih</label><br>
+                                    <input type="radio" id="warna3" placeholder="warna" name="warna" value="3">
+                                    <label for="warna3">Hijau</label><br>
+                                    <input type="radio" id="warna4" placeholder="warna" name="warna" value="4">
+                                    <label for="warna4">Biru</label><br>
+                                    <input type="radio" id="warna5" placeholder="warna" name="warna" value="5">
+                                    <label for="warna5">Merah</label><br>
+                                    <input type="radio" id="warna6" placeholder="warna" name="warna" value="6">
+                                    <label for="warna6">Kuning</label><br>
+                                    <input type="radio" id="warna7" placeholder="warna" name="warna" value="7">
+                                    <label for="warna7">Orange</label><br>
+                                    <input type="radio" id="warna82" placeholder="warna" name="warna" value="8">
+                                    <label for="warna8">Silver</label><br>
+                                    <input type="radio" id="warna9" placeholder="warna" name="warna" value="9">
+                                    <label for="warna9">Coklat</label><br>
+                                    <input type="radio" id="warna10" placeholder="warna" name="warna" value="10">
+                                    <label for="warna10">Hitam Transparan</label><br>
+                                    <input type="radio" id="warna11" placeholder="warna" name="warna" value="11">
+                                    <label for="warna11">Putih Transparan</label><br>
+                                    <input type="radio" id="warna12" placeholder="warna" name="warna" value="12">
+                                    <label for="warna12">Biru Transparan</label><br>
+                                    <input type="radio" id="warna13" placeholder="warna" name="warna" value="13">
+                                    <label for="warna13">Custom (Isi di keterangan)</label>
                                 </div>
                                 <div class="grid-item">
-                                    <b>Finishing</b>
+                                    <b>Casing</b>
                                     <br><br>
-                                    <input type="radio" id="finish1" placeholder="finishing" name="finishing" value="1">
-                                    <label for="finish1">Tidak ada</label><br>
-                                    <input type="radio" id="finish2" placeholder="finishing" name="finishing" value="2">
-                                    <label for="finish2">Urutkan</label><br>
-                                    <input type="radio" id="finish3" placeholder="finishing" name="finishing" value="3">
-                                    <label for="finish3">Label Gosok</label><br>
-                                    <input type="radio" id="finish4" placeholder="finishing" name="finishing" value="4">
-                                    <label for="finish4">Plong Oval</label><br>
-                                    <input type="radio" id="finish5" placeholder="finishing" name="finishing" value="5">
-                                    <label for="finish5">Plong Bulat</label><br>
-                                    <input type="radio" id="finish6" placeholder="finishing" name="finishing" value="6">
-                                    <label for="finish6">Copy Data RFID</label><br>
-                                    <input type="radio" id="finish7" placeholder="finishing" name="finishing" value="7">
-                                    <label for="finish7">Emboss Silver</label><br>
-                                    <input type="radio" id="finish8" placeholder="finishing" name="finishing" value="8">
-                                    <label for="finish8">Emboss Gold</label><br>
-                                    <input type="radio" id="finish9" placeholder="finishing" name="finishing" value="9">
-                                    <label for="finish9">Panel</label><br>
-                                    <input type="radio" id="finish10" placeholder="finishing" name="finishing" value="10">
-                                    <label for="finish10">Hot Print</label><br>
-                                    <input type="radio" id="finish11" placeholder="finishing" name="finishing" value="11">
-                                    <label for="finish11">Swipe</label><br>
+                                    <input type="radio" id="casing1" placeholder="casing" name="casing" value="1">
+                                    <label for="casing1">Casing ID Card Acrylic</label><br>
+                                    <input type="radio" id="casing2" placeholder="casing" name="casing" value="2">
+                                    <label for="casing2">Casing ID Card Solid</label><br>
+                                    <input type="radio" id="casing3" placeholder="casing" name="casing" value="3">
+                                    <label for="casing3">Casing ID Card Karet</label><br>
+                                    <input type="radio" id="casing4" placeholder="casing" name="casing" value="4">
+                                    <label for="casing4">Casing ID Card Kulit</label>
                                 </div>
                                 <div class="grid-item">
-                                    <b>Function</b>
+                                    <b>Varian Casing Karet</b>
                                     <br><br>
-                                    <input type="radio" id="function1" placeholder="function" name="function" value="1">
-                                    <label for="function1">Print Thermal</label><br>
-                                    <input type="radio" id="function2" placeholder="function" name="function" value="2">
-                                    <label for="function2">Scan Barcode</label><br>
-                                    <input type="radio" id="function3" placeholder="function" name="function" value="3">
-                                    <label for="function3">Swipe Magnetic</label><br>
-                                    <input type="radio" id="function4" placeholder="function" name="function" value="4">
-                                    <label for="function4">Tap RFID</label>
+                                    <input type="radio" id="ck1" placeholder="ck" name="ck" value="1">
+                                    <label for="ck1">Kirim Produk</label><br>
+                                    <input type="radio" id="ck2" placeholder="ck" name="ck" value="2">
+                                    <label for="ck2">Ambil Sendiri</label><br>
+                                    <input type="radio" id="ck3" placeholder="ck" name="ck" value="3">
+                                    <label for="ck3">Kirim Produk</label><br>
+                                    <input type="radio" id="ck4" placeholder="ck" name="ck" value="4">
+                                    <label for="ck4">Ambil Sendiri</label>
                                 </div>
                                 <div class="grid-item">
-                                    <b>Packaging</b>
+                                    <b>Logo Resin</b>
                                     <br><br>
-                                    <input type="radio" id="packaging1" placeholder="packaging" name="packaging" value="1">
-                                    <label for="packaging1">Plastik 1 on 1</label><br>
-                                    <input type="radio" id="packaging2" placeholder="packaging" name="packaging" value="2">
-                                    <label for="packaging2">Plastik Terpisah</label><br>
-                                    <input type="radio" id="packaging3" placeholder="packaging" name="packaging" value="3">
-                                    <label for="packaging3">Box Kartu Nama</label><br>
-                                    <input type="radio" id="packaging4" placeholder="packaging" name="packaging" value="4">
-                                    <label for="packaging4">Box Putih</label><br>
-                                    <input type="radio" id="packaging5" placeholder="packaging" name="packaging" value="5">
-                                    <label for="packaging5">Small UCARD</label><br>
-                                    <input type="radio" id="packaging6" placeholder="packaging" name="packaging" value="6">
-                                    <label for="packaging6">Small Maxi UCARD</label><br>
-                                    <input type="radio" id="packaging7" placeholder="packaging" name="packaging" value="7">
-                                    <label for="packaging7">Large UCARD</label><br>
-                                    <input type="radio" id="packaging8" placeholder="packaging" name="packaging" value="8">
-                                    <label for="packaging8">Large Maxi UCARD</label>
+                                    <input type="radio" id="lr" placeholder="lr" name="lr" value="1">
+                                    <label for="lr">Kirim Produk</label>
+                                </div>
+                                <div class="grid-item">
+                                    <b>Penjepit Buaya</b>
+                                    <br><br>
+                                    <input type="radio" id="pb1" placeholder="pb" name="pb" value="1">
+                                    <label for="pb1">Penjepit Buaya Besi</label><br>
+                                    <input type="radio" id="pb2" placeholder="pb" name="pb" value="2">
+                                    <label for="pb2">Penjepit Buaya Plastik</label>
                                 </div>
                                 <div class="grid-item">
                                     <b>Ambil/Kirim</b>
