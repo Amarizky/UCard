a1ffcaa3da9276e8da3152a2cc0bc73755dd5fdf
Amarizky Yoga Pratama <amarizky02@gmail.com>
2022-04-08 00:35:40 +0700

    kustomisasi di ubah status Fixes #139

--- application/views/admin/V_detail.php
+++ application/views/admin/V_detail.php
@@ -693,7 +693,7 @@ $y = $this->db->query("SELECT * FROM tbl_product AS p JOIN tbl_transaksi AS t ON
 ?>
 
 <div class="modal fade" id="status_update" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLongTitle">Ubah Status</h5>
